ac790c28e198b3e47554a8f6cda965aa3d7fa377
Anousone Kaseumsouk, Jean Jia and Lan Dam <jean@>
2018-08-14 10:32:32 -0700

    Add code to RecipeBox applicaiton.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe Box';
+  newRecipe: Recipe[] = [new Recipe('Fried Rice', ['Salt', 'Soy Sauce', 'Osyter Sauce', 'Rice', 'Egg', 'Green Onion'], ['Add oil to wok', 'Stir-fry egg and remove', 'Stir-fry protein and remove', 'Stir-fry vegetables', 'Add room temp rice and mix', 'Add: soy sauce, oyster sauce, white pepper', 'Salt to taste', 'Top with green onions and egg.'])]
 }
